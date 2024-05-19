d5666dbf35ab77f28ed7ebb6ed9598d683127766
Will Sturgeon <willstrgn@gmail.com>
2024-05-18 23:00:20 -0400

    Add weight decay and pre-normalize by RMS

--- damper/damper.py
+++ damper/damper.py
@@ -7,87 +7,74 @@ from jaxtyping import Array, Float, PyTree
 
 
 @check_and_compile()
-def standard_deviation_loss(
-    standard_deviations: Float[Array, "*batch"],
+def rms_loss(
+    rms: Float[Array, "*batch"],
     xs: Float[Array, "*batch"],
     epsilon: Float[Array, ""],
 ) -> Float[Array, ""]:
     """
-    Loss that, over time, precisely computes standard deviation.
+    Loss that, over time, precisely computes root-mean-squared.
     Assumes a mean of zero.
     """
-    actual_variances = jnp.square(xs)  # zero-mean
-    variances = jnp.square(standard_deviations)
-    squared_std_error = jnp.square(actual_variances - variances)
-    relative_error = squared_std_error / (epsilon + lax.stop_gradient(variances))
+    actual_mean_squared = jnp.square(xs)  # zero-mean
+    mean_squared = jnp.square(rms)
+    squared_rms_error = jnp.square(actual_mean_squared - mean_squared)
+    relative_error = squared_rms_error / (epsilon + lax.stop_gradient(mean_squared))
     return jnp.sum(relative_error)
 
 
 @check_and_compile()
-def standard_deviation_grad_exact(
-    standard_deviations: Float[Array, "*batch"],
+def rms_grad_exact(
+    rms: Float[Array, "*batch"],
     xs: Float[Array, "*batch"],
     epsilon: Float[Array, ""],
 ) -> Float[Array, "*batch"]:
     """
     Directly compute the gradient of the above function in closed form.
     """
-    actual_variances = jnp.square(xs)  # zero-mean
-    variances = jnp.square(standard_deviations)
+    actual_mean_squared = jnp.square(xs)  # zero-mean
+    mean_squared = jnp.square(rms)
 
-    # # Loss w.r.t. variance is easy:
-    # dLdv = 2 * (variances - actual_variances) / (epsilon + lax.stop_gradient(variances))
-    # return 2 * standard_deviations * dLdv
+    # # Loss w.r.t. mean_squared is easy:
+    # dLdv = 2 * (mean_squared - actual_mean_squared) / (epsilon + lax.stop_gradient(mean_squared))
+    # return 2 * rms * dLdv
 
     return (
         # 4 *
-        standard_deviations
-        * (variances - actual_variances)
-        / (epsilon + lax.stop_gradient(variances))
+        rms
+        * (mean_squared - actual_mean_squared)
+        / (epsilon + lax.stop_gradient(mean_squared))
     )
 
 
 @check_and_compile()
-def standard_deviation_grad_approx(
-    standard_deviations: Float[Array, "*batch"],
+def rms_grad_approx(
+    rms: Float[Array, "*batch"],
     xs: Float[Array, "*batch"],
     epsilon: Float[Array, ""],
 ) -> Float[Array, "*batch"]:
     """
     Directly compute the gradient of the above function in closed form.
     """
-    actual_variances = jnp.square(xs)  # zero-mean
-    variances = jnp.square(standard_deviations)
+    actual_mean_squared = jnp.square(xs)  # zero-mean
+    mean_squared = jnp.square(rms)
 
-    # # Loss w.r.t. variance is easy:
-    # dLdv = 2 * (variances - actual_variances) / (epsilon + lax.stop_gradient(variances))
-    # return 2 * standard_deviations * dLdv
+    # # Loss w.r.t. mean_squared is easy:
+    # dLdv = 2 * (mean_squared - actual_mean_squared) / (epsilon + lax.stop_gradient(mean_squared))
+    # return 2 * rms * dLdv
 
-    return (variances - actual_variances) / (
-        epsilon + jnp.abs(lax.stop_gradient(standard_deviations))
+    return (mean_squared - actual_mean_squared) / (
+        epsilon + jnp.abs(lax.stop_gradient(rms))
     )
 
 
 @check_and_compile()
 def normalize(
     xs: Float[Array, "*batch"],
-    standard_deviations: Float[Array, "*batch"],
+    rms: Float[Array, "*batch"],
     epsilon: Float[Array, ""],
 ) -> Float[Array, "*batch"]:
-    return xs / (epsilon + standard_deviations)
-
-
-@check_and_compile()
-def normalized_dot_product(
-    a: Float[Array, "*batch"],
-    b: Float[Array, "*batch"],
-    standard_deviations: Float[Array, "*batch"],
-    epsilon: Float[Array, ""],
-) -> Float[Array, "*batch"]:
-    # TODO: variance instead of dividing by std twice
-    a = normalize(a, standard_deviations, epsilon)
-    b = normalize(b, standard_deviations, epsilon)
-    return a * b  # no sum: treat each entry independently
+    return xs / (epsilon + rms)
 
 
 @check_and_compile()
@@ -96,38 +83,33 @@ def update_tensor(
     current_grad: Float[Array, "*batch"],
     previous_grad: Float[Array, "*batch"],
     lr: Float[Array, "*batch"],
-    stds: Float[Array, "*batch"],
+    rms: Float[Array, "*batch"],
     ideal_dot_prod: Float[Array, ""],
     sensitivity: Float[Array, ""],
-    std_update: Float[Array, ""],
+    rms_update: Float[Array, ""],
+    weight_decay: Float[Array, ""],
     epsilon: Float[Array, ""],
 ) -> Tuple[
     Float[Array, "*batch"],
     Float[Array, "*batch"],
     Float[Array, "*batch"],
 ]:
+    # Compute root-mean-squared error (but don't update it yet):
+    # dLds = grad(rms_loss)(rms, current_grad, epsilon)
+    dLds = rms_grad_approx(rms, current_grad, epsilon)
+
+    # Normalize gradients by their root-mean-squared:
+    current_grad = normalize(current_grad, rms, epsilon)
+    previous_grad = normalize(previous_grad, rms, epsilon)
+
+    # Update root-mean-squared (now that we've used the previous one for outliers):
+    rms = rms - rms_update * dLds
+
     # If the dot product of this gradient and the last is negative,
     # that means we're oscillating, and the more negative, the worse:
-    dot_prod = normalized_dot_product(
-        current_grad,
-        previous_grad,
-        stds,
-        epsilon,
-    )
+    dot_prod = current_grad * previous_grad  # already normalized
 
-    # Compute standard deviation error (but don't update it yet):
-    # dLds = grad(standard_deviation_loss)(stds, current_grad, epsilon)
-    dLds = standard_deviation_grad_approx(stds, current_grad, epsilon)
-
-    # # Toss out outliers:
-    # normalized = current_grad / (epsilon + stds)
-    # sigma = jnp.abs(normalized)
-    # current_grad = jnp.where(sigma < 3, current_grad, 0)
-
-    # Update standard deviation (now that we've used the previous one for outliers):
-    stds = stds - std_update * dLds
-
-    # Adjust learning rate to keep dot product approximately `ideal_covariance`:
+    # Adjust learning rate to keep dot product approximately `ideal_dot_prod`:
     dot_prod = jnp.minimum(1, dot_prod)
     error = dot_prod - ideal_dot_prod
     exponent = sensitivity * error
@@ -144,7 +126,7 @@ def update_tensor(
     lr = jnp.maximum(lr, epsilon)
 
     # Update parameters and return:
-    return parameters - lr * current_grad, lr, stds
+    return weight_decay * parameters - lr * current_grad, lr, rms
 
 
 @check_and_compile()
@@ -153,10 +135,11 @@ def update(
     current_grad: PyTree[Float[Array, "..."]],
     previous_grad: PyTree[Float[Array, "..."]],
     lr: PyTree[Float[Array, "..."]],
-    stds: PyTree[Float[Array, "..."]],
+    rms: PyTree[Float[Array, "..."]],
     ideal_dot_prod: Float[Array, ""],
     sensitivity: Float[Array, ""],
-    std_update: Float[Array, ""],
+    rms_update: Float[Array, ""],
+    weight_decay: Float[Array, ""],
     epsilon: Float[Array, ""],
 ) -> Tuple[
     PyTree[Float[Array, "..."]],
@@ -171,13 +154,14 @@ def update(
                 *args,
                 ideal_dot_prod,
                 sensitivity,
-                std_update,
+                rms_update,
+                weight_decay,
                 epsilon,
             ),
             parameters,
             current_grad,
             previous_grad,
             lr,
-            stds,
+            rms,
         ),
     )
