Fix normalization bug

damper/damper.py
@@ -93,6 +93,7 @@ def update_tensor(
     Float[Array, "*batch"],
     Float[Array, "*batch"],
     Float[Array, "*batch"],
+    Float[Array, "*batch"],
 ]:
     # Compute root-mean-squared error (but don't update it yet):
     # dLds = grad(rms_loss)(rms, current_grad, epsilon)
@@ -100,7 +101,7 @@ def update_tensor(
 
     # Normalize gradients by their root-mean-squared:
     current_grad = normalize(current_grad, rms, epsilon)
-    previous_grad = normalize(previous_grad, rms, epsilon)
+    # PREVIOUS GRADIENT IS ALREADY NORMALIZED: DO NOT DIVIDE TWICE
 
     # Update root-mean-squared (now that we've used the previous one for outliers):
     rms = rms - rms_update * dLds
@@ -126,7 +127,7 @@ def update_tensor(
     lr = jnp.maximum(lr, epsilon)
 
     # Update parameters and return:
-    return weight_decay * parameters - lr * current_grad, lr, rms
+    return weight_decay * parameters - lr * current_grad, current_grad, lr, rms
 
 
 @check_and_compile()
@@ -145,10 +146,11 @@ def update(
     PyTree[Float[Array, "..."]],
     PyTree[Float[Array, "..."]],
     PyTree[Float[Array, "..."]],
+    PyTree[Float[Array, "..."]],
 ]:
     return tree.transpose(
         outer_treedef=tree.structure(parameters),
-        inner_treedef=tree.structure(("*", "*", "*")),
+        inner_treedef=tree.structure(("*", "*", "*", "*")),
         pytree_to_transpose=tree.map(
             lambda *args: update_tensor(
                 *args,

test.py
@@ -10,69 +10,30 @@ key = jrnd.PRNGKey(42)
 BATCHES = [1, 2, 3]
 
 
-def prop_standard_deviation_loss_gradient_exact(stds, xs, eps):
-    f = damper.standard_deviation_grad_exact
-    g = grad(damper.standard_deviation_loss)
-    args = stds, xs, eps
+def prop_rms_loss_gradient_approx(rms, xs, eps):
+    f = damper.rms_grad_approx
+    g = grad(damper.rms_loss)
+    args = rms, xs, eps
     args = tuple([jnp.array(arg) for arg in args])
     y = g(*args)
     z = 4 * f(*args)
-    roundoff = jnp.maximum(jnp.maximum(jnp.square(stds), jnp.square(xs)) * 1e-5, 1e-8)
+    roundoff = jnp.maximum(jnp.maximum(jnp.square(rms), jnp.square(xs)) * 1e-5, 1e-8)
     if jnp.all(jnp.isfinite(y)) and jnp.all(jnp.isfinite(z)):
         assert jnp.all(
-            jnp.abs(y - jnp.sign(stds) * z) < roundoff
+            jnp.abs(y - jnp.sign(rms) * z) < roundoff
         ), f"\n\n{y}\n=/=\n{z}\n"
 
 
-def test_standard_deviation_loss_gradient_exact_1():
-    prop_standard_deviation_loss_gradient_exact(
+def test_rms_loss_gradient_approx_1():
+    prop_rms_loss_gradient_approx(
         jnp.zeros(BATCHES, dtype=jnp.float64),
         jnp.zeros(BATCHES, dtype=jnp.float64),
         jnp.zeros([], dtype=jnp.float32),
     )
 
 
-def test_standard_deviation_loss_gradient_exact_2():
-    prop_standard_deviation_loss_gradient_exact(
-        jnp.ones(BATCHES, dtype=jnp.float64),
-        jnp.zeros(BATCHES, dtype=jnp.float64),
-        jnp.zeros([], dtype=jnp.float32),
-    )
-
-
-@given(
-    hnp.arrays(np.float64, BATCHES),  # type: ignore[call-overload]
-    hnp.arrays(np.float64, BATCHES),  # type: ignore[call-overload]
-    hnp.arrays(np.float32, []),  # type: ignore[call-overload]
-)
-def test_standard_deviation_loss_gradient_exact_prop(stds, xs, eps):
-    prop_standard_deviation_loss_gradient_exact(stds, xs, eps)
-
-
-def prop_standard_deviation_loss_gradient_approx(stds, xs, eps):
-    f = damper.standard_deviation_grad_approx
-    g = grad(damper.standard_deviation_loss)
-    args = stds, xs, eps
-    args = tuple([jnp.array(arg) for arg in args])
-    y = g(*args)
-    z = 4 * f(*args)
-    roundoff = jnp.maximum(jnp.maximum(jnp.square(stds), jnp.square(xs)) * 1e-5, 1e-8)
-    if jnp.all(jnp.isfinite(y)) and jnp.all(jnp.isfinite(z)):
-        assert jnp.all(
-            jnp.abs(y - jnp.sign(stds) * z) < roundoff
-        ), f"\n\n{y}\n=/=\n{z}\n"
-
-
-def test_standard_deviation_loss_gradient_approx_1():
-    prop_standard_deviation_loss_gradient_approx(
-        jnp.zeros(BATCHES, dtype=jnp.float64),
-        jnp.zeros(BATCHES, dtype=jnp.float64),
-        jnp.zeros([], dtype=jnp.float32),
-    )
-
-
-def test_standard_deviation_loss_gradient_approx_2():
-    prop_standard_deviation_loss_gradient_approx(
+def test_rms_loss_gradient_approx_2():
+    prop_rms_loss_gradient_approx(
         jnp.ones(BATCHES, dtype=jnp.float64),
         jnp.zeros(BATCHES, dtype=jnp.float64),
         jnp.zeros([], dtype=jnp.float32),
@@ -83,9 +44,9 @@ def test_standard_deviation_loss_gradient_approx_2():
     hnp.arrays(np.float64, BATCHES),  # type: ignore[call-overload]
     hnp.arrays(np.float64, BATCHES),  # type: ignore[call-overload]
 )
-def test_standard_deviation_loss_gradient_approx_prop(stds, xs):
-    prop_standard_deviation_loss_gradient_approx(
-        stds,
+def test_rms_loss_gradient_approx_prop(rms, xs):
+    prop_rms_loss_gradient_approx(
+        rms,
         xs,
         jnp.zeros([], dtype=jnp.float32),
     )
@@ -97,33 +58,43 @@ def test_run_simple():
     init_loss = loss(p)
 
     lr = jnp.full_like(p, 0.01)
-    stds = jnp.ones_like(p)
-    sensitivity = jnp.array(0.05, dtype=jnp.float32)
-    epsilon = jnp.array(1e-8, dtype=jnp.float32)
+    rms = jnp.ones_like(p)
+    ideal_dot_prod = jnp.array(0.0, dtype=jnp.float32)
+    sensitivity = jnp.array(0.01, dtype=jnp.float32)
+    rms_update = jnp.array(0.001, dtype=jnp.float32)
+    weight_decay = jnp.array(0.999, dtype=jnp.float32)
+    epsilon = jnp.array(1e-5, dtype=jnp.float32)
 
     dLd = grad(loss)
 
     dLdp = dLd(p)
-    p, lr, stds = damper.update(
+    p, last_dLdp, lr, rms = damper.update(
         p,
         dLdp,
         jnp.zeros_like(p),
         lr,
-        stds,
+        rms,
+        ideal_dot_prod,
         sensitivity,
+        rms_update,
+        weight_decay,
         epsilon,
     )
     assert loss(p) < init_loss
 
-    last_dLdp = dLdp
-    dLdp = dLd(p)
-    p, lr, stds = damper.update(
-        p,
-        dLdp,
-        last_dLdp,
-        lr,
-        stds,
-        sensitivity,
-        epsilon,
-    )
+    for _ in range(100):
+        dLdp = dLd(p)
+        p, last_dLdp, lr, rms = damper.update(
+            p,
+            dLdp,
+            last_dLdp,
+            lr,
+            rms,
+            ideal_dot_prod,
+            sensitivity,
+            rms_update,
+            weight_decay,
+            epsilon,
+        )
+
     assert loss(p) < init_loss